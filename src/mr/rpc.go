c5eb6d4a37daa69b70f6053369a743243cac9625
Henlium <henlium@proton.me>
2023-02-27 23:25:36 +0800

    Move task-related definitions to coordinator

--- src/mr/rpc.go
+++ src/mr/rpc.go
@@ -17,21 +17,6 @@ type SingleInt struct {
 	Value int
 }
 
-type TaskType int
-
-const (
-	WaitForTask TaskType = iota
-	NoTask
-	MapTask
-	ReduceTask
-)
-
-type Task struct {
-	Type   TaskType
-	Number int    // the id of a m or r task
-	Input  string // the filename/path of the input file
-}
-
 // Cook up a unique-ish UNIX-domain socket name
 // in /var/tmp, for the coordinator.
 // Can't use the current directory since
